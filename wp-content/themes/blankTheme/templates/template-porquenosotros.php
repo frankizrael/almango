9c3f04775d4ac305fad03dbc5b744196f9a3fde9
frankizrael <frankdelarosacampos@gmail.com>
2020-08-28 16:45:19 -0500

    fixed

--- wp-content/themes/blankTheme/templates/template-porquenosotros.php
+++ wp-content/themes/blankTheme/templates/template-porquenosotros.php
@@ -20,25 +20,26 @@ $idhome = get_option( 'page_on_front' );
 			</div>	
 		</div>
 	</div>	
+	<section id="descuento" class="descuentos" style="background: transparent linear-gradient(90deg, #27F4CA 0%, #BD90FF 62%, #960FEF 100%) 0% 0% no-repeat padding-box;">
+		<div class="decorative_left">
+			<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/coupon_0.svg" style="transition-delay: 0s;">
+			<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/coupon_1.svg" style="transition-delay: 0.2s;">
+			<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/coupon_2.svg" style="transition-delay: 0.4s;">
+			<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/coupon_3.svg" style="transition-delay: 0.6s;">
+			<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/coupon_4.svg" style="transition-delay: 0.8s;">
+		</div>
+		<div class="x-container">
+			<h2><?php the_field('descuentos_title',$idhome); ?></h2>
+			<p><?php the_field('descuentos_subtitle',$idhome); ?></p>
+		</div>
+		<div class="decorative_right">
+			<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/discount.svg" style="transition-delay: 1s;">
+			<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/discount-1.svg" style="transition-delay: 1.2s;">
+			<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/discount-2.svg" style="transition-delay: 1.4s;">
+			<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/discount-3.svg" style="transition-delay: 1.6s;">
+		</div>
+	</section>
 </section>
-<section id="descuento" class="descuentos" style="background: transparent linear-gradient(90deg, #27F4CA 0%, #BD90FF 62%, #960FEF 100%) 0% 0% no-repeat padding-box;">
-	<div class="decorative_left">
-		<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/coupon_0.svg" style="transition-delay: 0s;">
-		<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/coupon_1.svg" style="transition-delay: 0.2s;">
-		<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/coupon_2.svg" style="transition-delay: 0.4s;">
-		<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/coupon_3.svg" style="transition-delay: 0.6s;">
-		<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/coupon_4.svg" style="transition-delay: 0.8s;">
-	</div>
-	<div class="x-container">
-		<h2><?php the_field('descuentos_title',$idhome); ?></h2>
-		<p><?php the_field('descuentos_subtitle',$idhome); ?></p>
-	</div>
-	<div class="decorative_right">
-		<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/discount.svg" style="transition-delay: 1s;">
-		<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/discount-1.svg" style="transition-delay: 1.2s;">
-		<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/discount-2.svg" style="transition-delay: 1.4s;">
-		<img src="<?php echo get_template_directory_uri(); ?>/src/assets/img/discount-3.svg" style="transition-delay: 1.6s;">
-	</div>
-</section>
+
 <?php get_footer();
 
